1c0bcbd774ea716bf2239f6d3a4d6b81f7a66c20
heavensbee001 <heavensbee001@gmail.com>
2022-11-19 00:05:02 +0100

    Upload contracts

--- spark-contracts/README.md
+++ spark-contracts/README.md
@@ -1,13 +1,5 @@
-# Sample Hardhat Project
+## Some info
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+I used remix to develop the contract. You can find a popy of the used .sol files on /contracts folder.
 
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
-```
+You can also import the remix-backup.zip file on remix.
--- spark-contracts/contracts/SharedStructs.sol
+++ spark-contracts/contracts/SharedStructs.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.3;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
@@ -20,11 +20,16 @@ library SharedStructs {
         Balance currentBalance;
         Balance[] historyBalance;
     }
+    struct Preferences {
+        uint sourceIndex;
+        uint percentage;
+    }
     struct User {
         string displayName;
         string meterID;
         uint pendingUSDBalance;
         Balance[] pendingBalance;
         Balance[] historyBalance;
+        Preferences[] preferences;
     }
 }
